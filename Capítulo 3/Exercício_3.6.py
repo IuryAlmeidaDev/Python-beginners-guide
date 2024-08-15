817af6f0b63c0c6ae9df6eda255c3c7b77435069
iury almeida <iury.colombo@gmail.com>
2024-08-15 19:16:00 -0300

    subindo questao 3.6

---
+++ Capítulo 3/Exercício_3.6.py
@@ -0,0 +1,7 @@
+# Escreva uma expressão que será utilizada para decidir se um aluno foi ou não aprovado. Para ser aprovado, todas as médias do aluno devem ser maiores que 7. Considere que o aluno cursa apenas três matérias, e que a nota de cada uma está armazenada nas seguintes variáveis: matéria1, matéria2 e matéria3.
+
+calculo = 7
+direito = 5
+economia = 10
+
+print (calculo >= 7 and direito >= 7 and economia >= 7)
