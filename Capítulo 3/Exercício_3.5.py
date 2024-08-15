2032840194a718cd0637cb74a6af098eb1d6b643
iury almeida <iury.colombo@gmail.com>
2024-08-15 19:08:15 -0300

    subindo questao 3.5

---
+++ Capítulo 3/Exercício_3.5.py
@@ -0,0 +1,13 @@
+# Calcule o resultado da expressão A > B and C or D, utilizando os valores da tabela a seguir.
+
+# A  | B |    C  |    D    |
+# 1  | 2 |  TRUE |  FALSE  |
+# 10 | 3 | FALSE |  FALSE  |
+# 5  | 1 |  TRUE |  TRUE   |
+
+a = 1
+b = 2
+c = True
+d = False
+
+print (a > b and c or d)
