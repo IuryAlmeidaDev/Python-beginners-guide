a0ad7fe70d285c2337d3b65999a4c303ca06949a
iury almeida <iury.colombo@gmail.com>
2024-08-15 19:01:53 -0300

    subindo questao 3.4

---
+++ Capítulo 3/Exercício_3.4.py
@@ -0,0 +1,6 @@
+#Escreva uma expressão para determinar se uma pessoa deve ou não pagar imposto. Considere que apgam imposto pessoas cujo salário é maior que R$ 1.200,00.
+
+salario = 1201
+idade = 18
+
+print(salario > 1200 and idade >= 18)
