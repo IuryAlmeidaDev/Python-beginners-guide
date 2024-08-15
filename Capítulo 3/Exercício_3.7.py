6ce3210579f5eb71ecd6278fd0ddd00d16c12dc7
iury almeida <iury.colombo@gmail.com>
2024-08-15 20:11:46 -0300

    questao 3.7

---
+++ Capítulo 3/Exercício_3.7.py
@@ -0,0 +1,7 @@
+# Faça um programa que peça números inteiros, Imprima a soma desses dois números na tela.
+
+numero1 = int(input("digite o primeiro numero: "))
+numero2 = int(input("digite o segundo numero: "))
+soma = numero1 + numero2
+
+print ("A soma dos dois numeros é %d" %soma)
