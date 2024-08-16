51db8ac5bc4c0b109874ab4b2357809209f70562
iury almeida <iury.colombo@gmail.com>
2024-08-16 19:38:12 -0300

    questao 4.9

---
+++ Capítulo 4/Exercício_4.9.py
@@ -0,0 +1,15 @@
+#
+
+valor_casa = float(input("Qual o valor da casa que voce deseja comprar? "))
+salario = float(input("Qual seu salario atual? "))
+anos = float(input("Em quantos anos que deseja parcelar? "))
+
+tempo = anos * 12
+valor_mensal = valor_casa / tempo
+
+limite = salario - (salario * 70 / 100)
+
+if valor_mensal > limite :
+    print("Voce nao pode comprar a casa pois a parcela ultrapassa 30 por cento do seu salário, que pena! ")
+else: 
+    print(f"O valor da sua parcela será {valor_mensal:.2f} reais, parábens")
