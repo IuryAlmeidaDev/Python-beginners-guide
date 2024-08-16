fc348e45e8788262c5367cf8e0f184f31a4408ce
iury almeida <iury.colombo@gmail.com>
2024-08-16 01:07:11 -0300

    questao 3.13

---
+++ Capítulo 3/Exercício_3.13.py
@@ -0,0 +1,10 @@
+#Escreva um programa que converta uma temperatura digitada em celsius PARA fahrenheit. A formula de conversao é f = 9 x C / 5 +32
+
+
+temperatura = int(input("Digite a temperatura atual: "))
+
+print ("Essa é a temperatura atual em graus celsius: %d" %temperatura)
+
+fahrenheit = (temperatura * 9 / 5) +32
+
+print ("Essa é a temperatura atual em fahrenheit: %d" % fahrenheit)
