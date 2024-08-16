f125d8e431a5d816e3590635ce02c733b5069591
iury almeida <iury.colombo@gmail.com>
2024-08-16 17:55:27 -0300

    questao 4.1

---
+++ Capítulo 4/Exercício_4.1.py
@@ -0,0 +1,3 @@
+# Analise o programa da listagem 4.2. Responda o que acontece se o primeiro eo segundo valores forem iguais? Explique.
+
+# NADA, POIS NAO EXISTE COMANDO PRA TRABALHAR ESSA HIPOTESE 
