6d893a971a0bc5cbfd99f54056628993ac6e131c
iury almeida <iury.colombo@gmail.com>
2024-08-17 10:52:52 -0300

    pergunta 5.1

--- Capítulo 4/Exercício_4.9.py
+++ Capítulo 4/Exercício_4.9.py
@@ -1,4 +1,5 @@
-#
+# Escreva um programa para aprovar o empréstimo bancário para compra de uma casa. O programa deve perguntar o valor da casa a comprar, o salário e a quantidade
+# de anos a pagar. O valor da prestação mensal nao pode ser superior a 30% do salário. Calcule o valor da prestação como sendo o valor da casa a comprar dividido pelo número de meses a pagar.
 
 valor_casa = float(input("Qual o valor da casa que voce deseja comprar? "))
 salario = float(input("Qual seu salario atual? "))
---
+++ Capítulo 5/Exercício_5.1.py
@@ -0,0 +1,7 @@
+# Modifique o programa pra exibir numero de 1 a 100
+
+numero = 0
+
+while numero < 100:
+    numero = numero + 1
+    print (numero)
