5b9948b62fd27ae294e221e707d0a0ed4481d41c
iury almeida <iury.colombo@gmail.com>
2024-08-21 14:10:07 -0400

    subindo

--- Capítulo 5/Exercício_5.8.py
+++ Capítulo 5/Exercício_5.8.py
@@ -1,2 +1,19 @@
-# modifique o programa da listagem 5.10 para que aceite respostas com letras maiúsculas e minusculas em todas as questoes
+""" Escreva um programa que leia dois números. Imprima o resultado da
+multiplicação do primeiro pelo segundo. Utilize apenas os operadores de soma e
+subtração para calcular o resultado. Lembre-se de que podemos entender a multiplicação de dois números como somas sucessivas de um deles. Assim, 4 × 5 = 5
++ 5 + 5 + 5 = 4 + 4 + 4 + 4 + 4.
+"""
+
+num1 = int(input("Digite o primeiro numero: "))
+num2 = int(input("Digite o segundo numero: "))
+
+contador = 1
+resultado = num1
+
+while contador < num2:
+    resultado += num1
+    contador += 1
+
+print(resultado)
+
 
