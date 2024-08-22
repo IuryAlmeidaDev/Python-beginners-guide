27bc6daed5621a0095135f6154b1d6af8063c6a3
iury almeida <iury.colombo@gmail.com>
2024-08-22 01:09:01 -0400

    add

--- Capítulo 5/Exercício_5.12.py
+++ Capítulo 5/Exercício_5.12.py
@@ -1,1 +1,20 @@
-# Altere o programa anterior de forma a perguntar também o valor depositado mensalmente. Esse valor será depositado no inicio de cada mês, e voce deve considerá-lo para cálculo de juros do mês seguinte.
+valor_inicial = int(input("Digite o valor inicial do deposito: "))
+taxa_de_juros = int(input("Digite o valor da taxa de juros: "))
+valor_mensal = float(input("Digite o valor que será depositado todo mês: "))
+
+mes = 1
+
+saldo = valor_inicial
+
+acumulador = 0
+
+
+while mes <= 24:
+    saldo += valor_mensal
+    juros_mes = (taxa_de_juros * saldo) /100
+    saldo += juros_mes
+    acumulador += juros_mes
+    print(f"Mês {mes}: Juros = R${juros_mes:.2f}, Saldo = R${saldo:.2f}")
+    mes += 1
+
+print(f"Total ganho com juros após 24 meses: R${acumulador:.2f}")
