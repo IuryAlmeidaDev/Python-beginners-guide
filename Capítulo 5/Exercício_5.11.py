36ab24931e19c531a6e644cc59fe42b77608c956
iury almeida <iury.colombo@gmail.com>
2024-08-22 01:00:26 -0400

    add

--- Capítulo 5/Exercício_5.11.py
+++ Capítulo 5/Exercício_5.11.py
@@ -1,3 +1,17 @@
-# nada ainda
+valor_inicial = int(input("Digite o valor inicial do deposito: "))
+taxa_de_juros = int(input("Digite o valor da taxa de juros: "))
 
+mes = 1
 
+saldo = valor_inicial
+
+acumulador = 0
+
+juros_mes = (taxa_de_juros * saldo) /100
+
+while mes <= 24:
+    juros_mes = (taxa_de_juros * saldo) /100
+    saldo += juros_mes
+    acumulador += juros_mes
+    print(f"Mês {mes}: Juros = R${juros_mes:.2f}, Saldo = R${saldo:.2f}")
+    mes += 1
